a39e57a3b2afe047edf5b616df234adf5e8c2e61
Mohammad Tepthon <98346135+zxaax@users.noreply.github.com>
2024-08-04 22:13:24 +0300

    Update Devmohammad.py

--- AnonX/plugins/Devmohammad.py
+++ AnonX/plugins/Devmohammad.py
@@ -15,15 +15,15 @@ from random import  choice, randint
 
                 
 @app.on_message(
-    command(["محمد","محمد مصطفى","حمد","المطور","Mohammad","حمودة"])
+    command(["أحمد موساشي","احمد موساشي","موساشي","المطور"])
     & filters.group
     & ~filters.edited
 )
 async def yas(client, message):
-    usr = await client.get_chat("PPF22")
+    usr = await client.get_chat("zxhduf")
     name = usr.first_name
     photo = await app.download_media(usr.photo.big_file_id)
-    await message.reply_photo(photo,       caption=f"**⩹━━𝗠𝗢𝗛𝗔𝗠𝗠𝗔𝗗 ‌♡⁩━━⩺\n\n‍ ¦dev :{name}\n ¦user :@{usr.username}\n ¦id :`{usr.id}`\n ¦bio :{usr.bio}\n\n**⩹━━𝗠𝗢𝗛𝗔𝗠𝗠𝗔𝗗 ‌♡⁩━━⩺**", 
+    await message.reply_photo(photo,       caption=f"**⩹━━موساشــي⁩━━⩺\n\n‍ ¦dev :{name}\n ¦user :@{usr.username}\n ¦id :`{usr.id}`\n ¦bio :{usr.bio}\n\n**⩹━موساشـي━━⩺**", 
     reply_markup=InlineKeyboardMarkup(
             [
                 [
