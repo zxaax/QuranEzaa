91040ea1d48e27f8ff99037ce232a57be3a2b0b7
Mohammad Tepthon <98346135+zxaax@users.noreply.github.com>
2024-08-05 00:51:16 +0300

    Update config.py

--- config.py
+++ config.py
@@ -16,17 +16,17 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID"))
 MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "QuranBot")
 
-OWNER_ID = list(map(int, getenv("OWNER_ID", "1260465030").split()))
+OWNER_ID = list(map(int, getenv("OWNER_ID", "6627097100").split()))
 
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Tepthonee/AlQuran")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/zxaax/QuranEzaa")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "QUQQ5")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/PPF22")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "tmemgqa10")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/zxaax")
 
 SUPPORT_HEHE = SUPPORT_GROUP.split("me/")[1]
 
