74bdea8f44372c7644707acd8e143ee307935347
Mohammad Tepthon <98346135+zxaax@users.noreply.github.com>
2024-08-07 17:32:50 +0300

    Update userbot.py

--- AnonX/core/userbot.py
+++ AnonX/core/userbot.py
@@ -49,7 +49,7 @@ class Userbot(Client):
             await self.one.start()
             try:
                 await self.one.join_chat("Tepthon")
-                await self.one.join_chat("Tepthon_Help")
+                await self.one.join_chat("Tepthon_Support")
             except:
                 pass
             assistants.append(1)
@@ -80,7 +80,7 @@ class Userbot(Client):
             await self.two.start()
             try:
                 await self.two.join_chat("Tepthon")
-                await self.two.join_chat("Tepthon_Help")
+                await self.two.join_chat("Tepthon_Support")
             except:
                 pass
             assistants.append(2)
@@ -111,7 +111,7 @@ class Userbot(Client):
             await self.three.start()
             try:
                 await self.three.join_chat("Tepthon")
-                await self.three.join_chat("Tepthon_Help")
+                await self.three.join_chat("Tepthon_Support")
             except:
                 pass
             assistants.append(3)
@@ -142,7 +142,7 @@ class Userbot(Client):
             await self.four.start()
             try:
                 await self.four.join_chat("Tepthon")
-                await self.four.join_chat("Tepthon_Help")
+                await self.four.join_chat("Tepthon_Support")
             except:
                 pass
             assistants.append(4)
@@ -173,7 +173,7 @@ class Userbot(Client):
             await self.five.start()
             try:
                 await self.five.join_chat("Tepthon")
-                await self.five.join_chat("Tepthon_Help")
+                await self.five.join_chat("Tepthon_Support")
             except:
                 pass
             assistants.append(5)
