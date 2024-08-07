0aee01de09063f9a096015e1fb0205d6974244af
Mohammad Tepthon <98346135+zxaax@users.noreply.github.com>
2024-08-07 17:15:53 +0300

    Update __init__.py

--- AnonX/__init__.py
+++ AnonX/__init__.py
@@ -1,37 +1,26 @@
-from AnonX.core.bot import AnonXBot
+from AnonX.core.bot import Anony
 from AnonX.core.dir import dirr
 from AnonX.core.git import git
 from AnonX.core.userbot import Userbot
-from AnonX.misc import dbb, heroku, sudo
-from aiohttp import ClientSession
+from AnonX.misc import dbb, heroku
 
 from .logging import LOGGER
 
-
 dirr()
-
 git()
-
 dbb()
-
 heroku()
 
-sudo()
-
-# Clients
-app = AnonXBot()
-
+app = Anony()
 userbot = Userbot()
 
 
 from .platforms import *
 
-YouTube = YouTubeAPI()
-Carbon = CarbonAPI()
-Spotify = SpotifyAPI()
 Apple = AppleAPI()
-Resso = RessoAPI()
+Carbon = CarbonAPI()
 SoundCloud = SoundAPI()
+Spotify = SpotifyAPI()
+Resso = RessoAPI()
 Telegram = TeleAPI()
-
-aiohttpsession = ClientSession()
+YouTube = YouTubeAPI()
