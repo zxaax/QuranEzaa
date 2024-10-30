3b6d8ce304479dbf531863a430f9f4ee0a24531f
Mohammad Tepthon <98346135+zxaax@users.noreply.github.com>
2024-10-30 20:36:32 +0300

    Update formatters.py

--- AnonX/utils/formatters.py
+++ AnonX/utils/formatters.py
@@ -1,13 +1,12 @@
-from typing import Union
-
-from pyrogram.types import Message
+import json
+import subprocess
 
 
 def get_readable_time(seconds: int) -> str:
     count = 0
     ping_time = ""
     time_list = []
-    time_suffix_list = ["s", "m", "h", "days"]
+    time_suffix_list = ["s", "ᴍ", "ʜ", "ᴅᴀʏs"]
     while count < 4:
         count += 1
         if count < 3:
@@ -61,10 +60,7 @@ async def alpha_to_int(user_id_alphabet: str) -> int:
 
 def time_to_seconds(time):
     stringt = str(time)
-    return sum(
-        int(x) * 60**i
-        for i, x in enumerate(reversed(stringt.split(":")))
-    )
+    return sum(int(x) * 60**i for i, x in enumerate(reversed(stringt.split(":"))))
 
 
 def seconds_to_min(seconds):
@@ -85,6 +81,67 @@ def seconds_to_min(seconds):
         elif s > 0:
             return "00:{:02d}".format(s)
     return "-"
+
+
+def speed_converter(seconds, speed):
+    if str(speed) == str("0.5"):
+        seconds = seconds * 2
+    if str(speed) == str("0.75"):
+        seconds = seconds + ((50 * seconds) // 100)
+    if str(speed) == str("1.5"):
+        seconds = seconds - ((25 * seconds) // 100)
+    if str(speed) == str("2.0"):
+        seconds = seconds - ((50 * seconds) // 100)
+    collect = seconds
+    if seconds is not None:
+        seconds = int(seconds)
+        d, h, m, s = (
+            seconds // (3600 * 24),
+            seconds // 3600 % 24,
+            seconds % 3600 // 60,
+            seconds % 3600 % 60,
+        )
+        if d > 0:
+            convert = "{:02d}:{:02d}:{:02d}:{:02d}".format(d, h, m, s)
+            return convert, collect
+        elif h > 0:
+            convert = "{:02d}:{:02d}:{:02d}".format(h, m, s)
+            return convert, collect
+        elif m > 0:
+            convert = "{:02d}:{:02d}".format(m, s)
+            return convert, collect
+        elif s > 0:
+            convert = "00:{:02d}".format(s)
+            return convert, collect
+    return "-"
+
+
+def check_duration(file_path):
+    command = [
+        "ffprobe",
+        "-loglevel",
+        "quiet",
+        "-print_format",
+        "json",
+        "-show_format",
+        "-show_streams",
+        file_path,
+    ]
+
+    pipe = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    out, err = pipe.communicate()
+    _json = json.loads(out)
+
+    if "format" in _json:
+        if "duration" in _json["format"]:
+            return float(_json["format"]["duration"])
+
+    if "streams" in _json:
+        for s in _json["streams"]:
+            if "duration" in s:
+                return float(s["duration"])
+
+    return "Unknown"
 
 
 formats = [
